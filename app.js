e2b04030903ef9bf17634a23cd2a387c9af7f6ee
Felipe Alencar <fsalencar@outlook.com>
2016-12-16 15:47:27 -0200

    Arrumando o pull request do pedro

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 'use strict';
 
-<<<<<<< HEAD
-app.listen(process.env.PORT || 3000,function(){
-=======
+//HEAD
 let app = require('./config/express')(),
     rotasProdutos = require('./app/routes/produtos')(app);
 
+
 app.listen(process.env.PORT || 3000, function() {
->>>>>>> abc3eaa332c2bcc56dd567976c5d635979143f5a
     console.log("Servidor on!");
 });
