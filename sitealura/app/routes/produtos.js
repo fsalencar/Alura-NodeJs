0167a66e5066d4e558253e8e6cb93a32b9ecd49e
Felipe Alencar <fsalencar@outlook.com>
2016-12-16 09:23:00 -0200

    Iniciando o banco de dados e colocando para funcionar!

--- sitealura/app/routes/produtos.js
+++ sitealura/app/routes/produtos.js
@@ -1,5 +1,19 @@
 module.exports = function(app) {
   app.get('/produtos',function(req,res){
-      res.render("produtos/lista")
+      var mysql = require ('mysql');
+      var connection = mysql.createConnection({
+          host : 'localhost',
+          user : 'root',
+          password : 'LipeOwna0000',
+          database : 'topper'
+      });
+
+      connection.query('select * from livros',function(err,result){
+          res.render('produtos/lista',{lista:result});
+      });
+
+      connection.end();
+      //consultando
+      //  res.render("produtos/lista");
   });
 }
--- sitealura/config/express.js
+++ sitealura/config/express.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 app.set('view engine','ejs');
+app.set('views','./app/views');
 
 module.exports = function() {
   return app;
